97c6db536413e47edcc4a8ea91f9ec01b5d6f425
GABRIEL SUAREZ ARAOZ <gabriel.suarez@alumnos.upm.es>
2024-06-06 00:18:34 +0200

    imagenes pruebas y script para ejecutar

--- main.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cabal v2-build
-.\dist-newstyle\build\x86_64-linux\ghc-8.8.4\haskell-0.1.0.0\x\haskell\build\haskell\haskell > src-exe\output.svg -w 400
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./dist-newstyle/build/x86_64-linux/ghc-8.8.4/haskell-0.1.0.0/x/haskell/build/haskell/haskell -o ./src-exe/output.svg -w 400
